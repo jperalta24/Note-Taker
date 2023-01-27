8db1a7cc8b9a8e4e4e15e0b982cf1601123ed383
Slender James <114044205+jperalta24@users.noreply.github.com>
2023-01-27 08:06:09 -0800

    added folders and files

---
+++ Develop/routes/html.js
@@ -0,0 +1,11 @@
+
+const notes = require('express').Router();
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+})
+
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
