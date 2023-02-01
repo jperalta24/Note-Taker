8dc431c88f25c1cee70ce5365d04aea47d3b17a2
Slender James <114044205+jperalta24@users.noreply.github.com>
2023-01-31 19:44:44 -0800

    debug

--- Develop/routes/api/notes.js
+++ Develop/routes/api/notes.js
@@ -1,17 +1,47 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../../helpers/fsUtils');
+const { readFromFile, readAndAppend, writeToFile } = require('../../helpers/fsUtils');
 const fs = require('fs');
-
+// const db = require('../../db/db.json');
 const uniqid = require('uniqid');
 
 
 
+
 notes.get('/', (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+    // readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+    fs.readFile('./db/db.json', (err,data) => {
+        if (err) throw err;
+        let db = JSON.parse(data);
+        res.json(db);
+    })
 });
 
 
 notes.post('/', (req, res) => {
+
+    // const newNote = req.body
+    // const {title, text} = req.body
+    // newNote.id = uniqid();
+
+    //  const { title, text } = req.body;
+
+    // if (req.body) {
+    //     const newNote = {
+    //         title,
+    //         text,
+    //         note_id: uniqid(),
+    //     };
+
+
+    // let db = JSON.parse(fs.readFile('./db/db.json'));
+
+    // db.push(newNote);
+
+    // fs.writeFile('./db/db.json', JSON.stringify(db));
+    // res.json(db);
+    // } else {
+    //     res.errored('error creating note')
+    // }
     console.log(req.body);
 
     const { title, text } = req.body;
@@ -29,22 +59,20 @@ notes.post('/', (req, res) => {
         res.errored('error creating new note');
     }
 
-
+    
 });
 
+notes.delete('/:note_id', (req, res) => {
+    const noteId = req.params.note_id;
+    readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) =>{
+        const result = json.filter((note) => {note.note_id !== noteId});
+        writeToFile('./db/db.json', result);
 
-
-
-
-
-
-
-
-
-
-
-
-
+        res.json('Note has been deleted')
+    })
+});
 
 
 module.exports = notes;
--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -3,6 +3,7 @@ const homeRoutes = require('./homeRoutes.js');
 const apiRoutes = require('./api');
 const app = express();
 
+// the order of the routes affects the endpoint when doing a GET request.
 app.use('/api', apiRoutes);
 app.use('/', homeRoutes); 
 
